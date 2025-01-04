fc6025b4c2eae920f98b52c8408991eed0a579eb
baban <babanba.n@gmail.com>
2025-01-04 17:45:51 +0900

    タグ表示周りの調整

--- docker/publify/config/application.rb
+++ docker/publify/config/application.rb
@@ -17,6 +17,12 @@ module Publify
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.action_view.sanitized_allowed_tags = ["a", "abbr", "acronym", "address", "b", "big", "blockquote", "br", "cite", "code", "dd", "del", "dfn", "div", "dl", "dt", "em", "h1", "h2", "h3", "h4", "h5", "h6", "hr", "i", "img", "ins", "kbd", "li", "mark", "ol", "p", "pre", "samp", "small", "span", "strong", "sub", "sup", "time", "tt", "ul", "var", "table", "tr"]
+    config.action_view.sanitized_allowed_attributes = ["abbr","alt","cite","class","datetime","height","href","lang","name","src","title","width","xml:lang"]
+    # 追加で使いたいタグをここで定義する
+    config.action_view.sanitized_allowed_tags += ['p', "blockcode", "div", "dl", "dt", "embed", "fieldset", "form", "hr", "iframe", "input", "ins", "kbd", "key", "label", "legend", "noscript", "object", "param", "q", "rb", "rp", "rt", "ruby", "script", "table", "tbody", "td", "th", "tr", "u"]
+    config.action_view.sanitized_allowed_attributes += ["id"]
   end
 
   # Load included libraries.
